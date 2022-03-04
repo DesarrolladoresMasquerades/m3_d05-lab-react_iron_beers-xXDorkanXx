90fc504b51fc020c4c1c40077a3fa3f6816babc3
Fer Sánchez <sangarfer@gmail.com>
2022-03-04 18:23:07 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import BeersListPage from "./pages/BeersListPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
+import CreateBeerPage from "./pages/CreateBeerPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/beers/:beerId" element={<BeerDetailsPage/>}/>
         <Route exact path="/random-beer" element={<RandomBeerPage/>}/>
+        <Route exact path="/new-beer" element={<CreateBeerPage/>}/>
         <Route exact path="/beers" element={<BeersListPage/>}/>
         <Route exact path="/" element={<HomePage/>}/>
       </Routes>
--- src/pages/BeersListPage.jsx
+++ src/pages/BeersListPage.jsx
@@ -6,15 +6,24 @@ import Header from "../components/Header";
 export default function BeersListPage(){
 
     const [beers, setBeers] = useState([]);
+    const [searchBarData, setSearchBarData] = useState("")
 
     useEffect(()=>{
         axios.get("https://ih-beers-api2.herokuapp.com/beers")
         .then((response)=>{setBeers(response.data)})
     }, [])
 
+    function filterBeers(event){
+        const value = event.target.value;
+        setSearchBarData(value);
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${value}`)
+        .then((response)=>{response.data.length ? setBeers(response.data) : setBeers([])})
+    }
+
     return(beers.length ?
         <section>
             <Header/>
+            <input className="search-bar" value={searchBarData} type="search" name="search-bar" onChange={filterBeers} />
             {beers.map((beer)=>{
                 return(
                     <Link key={beer._id} to={"/beers/" + beer._id}>
@@ -30,9 +39,17 @@ export default function BeersListPage(){
                 )
             })}
         </section>
+        : searchBarData.length ?
+        <section>
+            <Header/>
+            <input className="search-bar" value={searchBarData} type="search" name="search-bar" onChange={filterBeers} />
+            <img src="https://thumbs.dreamstime.com/b/sad-beer-cartoon-character-sad-beer-cartoon-character-vector-illustration-design-113397539.jpg" alt="loading-img" width={300}/>
+            <p>No beers found!</p>
+        </section>
         :
         <section>
             <Header/>
+            <input className="search-bar" value={searchBarData} type="search" name="search-bar" onChange={filterBeers} />
             <img src="https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif" alt="loading-img" width={300}/>
             <p>Loading...</p>
         </section>
